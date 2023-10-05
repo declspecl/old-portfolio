a24e246c00c2d9b794bd27cd9b20a754cb9d9279
declspecl <gavind2559@gmail.com>
2023-10-04 23:41:40 -0400

    initial commit

---
+++ src/Fonts.ts
@@ -0,0 +1,4 @@
+import { JetBrains_Mono, Inter } from "next/font/google";
+
+export const jbMono = JetBrains_Mono({ subsets: ["latin"], weight: "variable" });
+export const inter = Inter({ subsets: ["latin"], weight: "variable" });
