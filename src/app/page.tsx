f32458893c6eb7db456187661926f17d40e7ef0a
declspecl <gavind2559@gmail.com>
2023-10-05 18:40:13 -0400

    basic scaffolding of pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Hero from "@/components/Hero/Hero";
+import About from "@/components/About/About";
+import Skills from "@/components/Skills/Skills";
+import Contact from "@/components/Contact/Contact";
+import Projects from "@/components/Projects/Projects";
+
 export default function Home() {
 	return (
 		<div>
-
+			<Hero />
+			<About />
+			<Projects />
+			<Skills />
+			<Contact />
 		</div>
-	)
+	);
 }
