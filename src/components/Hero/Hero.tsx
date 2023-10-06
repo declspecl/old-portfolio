89a4068703ec30b09e4dea5edbf8c638c880bbb0
declspecl <gavind2559@gmail.com>
2023-10-06 00:18:53 -0400

    basic r3f setup - need to deep dive docs

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import TestBox from "./TestBox";
+import { Canvas } from "@react-three/fiber";
+
 export default function Hero() {
 	return (
-		<div>
-
+		<div className="w-screen h-screen">
+			<Canvas>
+				<perspectiveCamera fov={90} near={0.1} far={10} position={[0, 0, 5]} />
+				<ambientLight intensity={0.5} />
+				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+				<pointLight position={[-10, -10, -10]} />
+				<TestBox position={[-1.2, 0, 0]} />
+				<TestBox position={[1.2, 0, 0]} />
+			</Canvas>
 		</div>
 	);
 }
