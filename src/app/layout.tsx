e15d4599c2926c4dec17db58bba8ff50016ac339
declspecl <gavind2559@gmail.com>
2023-10-15 00:18:28 -0400

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { inter } from "@/Fonts";
-import type { Metadata } from "next"
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata =
 {
@@ -18,7 +19,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+
+				<Analytics />
+			</body>
 		</html>
 	)
 }
