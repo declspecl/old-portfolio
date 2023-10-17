daa1c8e37ddb55748901595df04e7fb0f2c7b471
declspecl <gavind2559@gmail.com>
2023-10-17 12:41:35 -0400

    implemented equal width and height about columns

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,58 +4,67 @@ import Section from "@/components/Layout/Section";
 import SectionText from "@/components/Typography/SectionText";
 
 export default function About() {
-	return (
-		<Section>
-			<SectionText id="about">About</SectionText>
+    return (
+        <Section>
+            <SectionText id="about">About</SectionText>
 
-			<div className={clsx(
+            <div className={clsx(
                 "flex flex-col-reverse gap-4",
-                "md:flex-row md:items-stretch"
-			)}>
-				<div className={clsx(
-					"flex flex-col gap-5",
-                    "md:flex-1"
-				)}>
-                   <h3 className={clsx(
-                       "text-3xl text-text-50",
-                       jbMono.className
-                   )}>
-                       I&apos;m <span className="text-primary-200">Gavin</span>, a student and software engineer.
-                   </h3>
+                "md:flex-row md:justify-center"
+            )}>
+                <div className={clsx(
+                    "flex flex-col gap-5",
+                    "md:flex-1",
+                    "xl:py-2 "
+                )}>
+                    <div className={clsx(
+                        "flex flex-col gap-3",
+                        jbMono.className
+                    )}>
+                        <h2 className="text-4xl text-primary-200 font-medium">
+                            Hello!
+                        </h2>
 
-					<div className={clsx(
-						"flex flex-col gap-3 text-text-50",
-						"text-lg",
-						"xl:text-xl",
-						inter.className
-					)}>
-						<p>
-							I have been programming for over 6 years, and it is by far my greatest passion.
-						</p>
+                        <h3 className={clsx(
+                            "text-2xl text-text-50"
+                        )}>
+                            I&apos;m Gavin, a student and software engineer.
+                        </h3>
+                    </div>
 
-						<p>
-							I went to an International Baccalaureate high school, graduating with a 4.2 GPA as a valedictorian and IB Diploma Candidate.
-							During this time, I took a higher level computer science class, achieving the highest score in my school.
-							Currently, I attend Oakland University as a Computer Science major.
-						</p>
+                    <div className={clsx(
+                        "flex flex-col gap-3 text-text-50",
+                        "text-lg",
+                        "xl:text-xl",
+                        inter.className
+                    )}>
+                        <p>
+                            I have been programming for over 6 years, and it is by far my greatest passion.
+                        </p>
 
-						<p>
-							Outside of academics, I enjoy playing video games, playing instruments, and, generally, learning.
-							I am always learning something, whether it be a new computer science concept, a new song, or a new skill.
-						</p>
-					</div>
-				</div>
+                        <p>
+                            I went to an International Baccalaureate high school, graduating with a 4.2 GPA as a valedictorian and IB Diploma Candidate.
+                            During this time, I took a higher level computer science class, achieving the highest score in my school.
+                            Currently, I attend Oakland University as a Computer Science major.
+                        </p>
 
-                <div className="flex-1">
-				    <img
-				    	src="/senior-pic.webp"
-				    	alt="My senior picture"
-				    	className={clsx(
-                            "w-full h-auto object-cover"
-				    	)}
-				    />
+                        <p>
+                            Outside of academics, I enjoy playing video games, playing instruments, and, generally, learning.
+                            I am always learning something, whether it be a new computer science concept, a new song, or a new skill.
+                        </p>
+                    </div>
                 </div>
-			</div>
-		</Section>
-	);
+
+                <div className="md:relative md:flex-1">
+                    <img
+                        src="/senior-pic.webp"
+                        alt="My senior picture"
+                        className={clsx(
+                            "md:absolute md:w-full md:h-full md:object-cover md:object-top"
+                        )}
+                    />
+                </div>
+            </div>
+        </Section>
+    );
 }
--- src/components/Layout/Centered.tsx
+++ src/components/Layout/Centered.tsx
@@ -10,7 +10,8 @@ export default function Centered({ children, className }: CenteredProps) {
 	return (
 		<div className={clsx(
 			"relative w-4/5 mx-auto z-20",
-			"xl:w-3/5",
+			"xl:w-8/12",
+            "2xl:w-3/5",
 			className
 		)}>
 			{children}
