79b27866bb1b3262b31635a51b3be383e64ad157
declspecl <gavind2559@gmail.com>
2023-10-22 02:25:23 -0400

    changed icon behavior again

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,24 +12,29 @@ export default function manifest(): MetadataRoute.Manifest {
 		icons: [
 			{
 				src: "/favicon.ico",
-				sizes: "48x48",
+				sizes: "32x32",
 				type: "image/x-icon"
 			},
 			{
-				src: "/favicon-32x32.png",
+				src: "/icon1.png",
+				sizes: "16x16",
+				type: "image/png"
+			},
+			{
+				src: "/icon2.png",
 				sizes: "32x32",
 				type: "image/png"
 			},
 			{
-				src: "/favicon-16x16.png",
-				sizes: "16x16",
+				src: "/icon3.png",
+				sizes: "192x192",
 				type: "image/png"
 			},
-            {
-                src: "/apple-touch-icon.png",
-                sizes: "180x180",
-                type: "image/png"
-            }
+			{
+				src: "/icon4.png",
+				sizes: "512x512",
+				type: "image/png"
+			}
 		]
 	}
 }
