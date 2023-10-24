36996e9edd2ccee2e7c555793003465e5f026c41
declspecl <gavind2559@gmail.com>
2023-10-24 14:10:33 -0400

    changed site listings to include www

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,12 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next"
  
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://gavindhondt.com',
+			url: "https://www.gavindhondt.com",
 			lastModified: new Date(),
-			changeFrequency: 'weekly',
-			priority: 1,
+			changeFrequency: "weekly",
+			priority: 1
 		}
 	]
 }
