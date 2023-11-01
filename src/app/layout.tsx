b400176861962967e5ec65d160c36d4dc533d347
declspecl <gavind2559@gmail.com>
2023-11-01 01:18:17 -0400

    upgraded to next 14 and updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import "./globals.css"
 import { inter } from "@/Fonts";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/react";
 
-export const metadata: Metadata =
-{
+export const metadata: Metadata = {
     title: "Gavin D'Hondt",
     description: "I'm Gavin D'Hondt. I'm a software engineer studying Computer Science at Oakland University. I'm actively seeking internship opportunities and freelance web development jobs.",
     authors: { name: "Gavin D'Hondt" },
-    keywords: ["Gavin D'Hondt", "Software engineer", "Student", "Rust", "C++", "React", "Developer", "Oakland University", "Michigan", "Programmer"],
+    keywords: ["Gavin D'Hondt", "Software engineer", "Student", "Rust", "C++", "React", "Developer", "Oakland University", "Michigan", "Programmer"]
+}
+
+export const viewport: Viewport = {
+    themeColor: "#16161b",
     colorScheme: "dark"
 }
 
